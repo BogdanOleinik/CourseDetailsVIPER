348ca908004d0b64059c7acad70a56f59019239d
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-04-02 13:25:34 +0300

    Initial Commit

---
+++ CourseDetailsVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CourseDetailsVIPER
+//
+//  Created by Олейник Богдан on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
